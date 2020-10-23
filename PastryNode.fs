5f069fdc20ca3817392df18fe769065fae3fac65
Adam Tamargo <tamargoadam@gmail.com>
2020-10-23 17:26:28 -0400

    add empty function

--- PastryNode.fs
+++ PastryNode.fs
@@ -2,11 +2,19 @@ module PastryNode
 
 open Akka.Actor
 open Akka.FSharp
+open System.Collections.Generic
+
+let findNextHop (routingTable: Map<int,string>) (destinationId: int) =
+    // TODO: return address of key closest to destinationId
+
+    0
 
 let ParticipantActor (mailbox : Actor<'a>)= 
     // TODO: instantiate mutable ID -> Address map
     // manage different kinds of messages based on contents
     // if message ment to end at this participant is recieved, send message with number of hops to registryActor
+
+    let routingTable = Map<int,string>
 
     let rec loop () = 
         actor {
